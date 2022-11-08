e3a375eb0e2db753793193cf631c6e80a30ca19b
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2022-11-08 20:26:44 +0600

    server created

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("torur server running")
+})
+
+app.listen(port, (req, res) => {
+    console.log(`server running ${port}`)
+})
+
+// pass: qYTNkxT5XpUy8rx5
+// user = genius-user
