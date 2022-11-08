93e7ffd167b0034fd325e39e0fe8135f86612732
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2022-11-08 21:01:11 +0600

    mongo db added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -7,6 +8,11 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors())
 app.use(express.json())
+
+
+//mongo db
+const uri = `mongodb+srv://${process.env.TOUR_USER}:${process.env.TOUR_PASSWORD}@cluster0.guw4vbk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 app.get("/", (req, res) => {
     res.send("tour server running")
@@ -18,3 +24,4 @@ app.listen(port, (req, res) => {
 
 // pass: qYTNkxT5XpUy8rx5
 // user = genius-user
+
