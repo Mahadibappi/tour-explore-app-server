84bfcf8b70d720d0913c817d57cb82aa4d43d9ce
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2022-11-09 11:42:32 +0600

    review post created

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try{
         const serviceCollection = client.db("tourUser").collection('tours');
+        const reviewCollection = client.db("tourUser").collection("review")
+
+
+        // get api 
         app.get('/services', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query);
@@ -30,6 +34,15 @@ async function run() {
             const service = await serviceCollection.findOne(query);
             res.send(service)
         })
+
+        //review api
+        app.post('/review', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result)
+        })
+
+
     }
     finally {
         
