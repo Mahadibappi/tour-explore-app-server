88e6f470fd56706a9bf69293b1f2000e10766868
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2022-11-10 15:30:49 +0600

    my review adde

---
+++ _deploy.steps.js
@@ -0,0 +1,6 @@
+/*
+1 . add vercel config
+2.add "build": "node index.js"
+3. install vercel
+
+ */
--- index.js
+++ index.js
@@ -1,62 +1,72 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const app = express()
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 //mongo db
 const uri = `mongodb+srv://${process.env.TOUR_USER}:${process.env.TOUR_PASSWORD}@cluster0.guw4vbk.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-// crud operations 
+// crud operations
 async function run() {
-    try{
-        const serviceCollection = client.db("tourUser").collection('tours');
-        const reviewCollection = client.db("tourUser").collection("review")
+  try {
+    const serviceCollection = client.db("tourUser").collection("tours");
+    const reviewCollection = client.db("tourUser").collection("review");
 
+    // get api
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
 
-        // get api 
-        app.get('/services', async (req, res) => {
-            const query = {}
-            const cursor = serviceCollection.find(query);
-            const services = await cursor.toArray()
-            res.send(services)
-        });
-        app.get("/services/:id", async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const service = await serviceCollection.findOne(query);
-            res.send(service)
-        })
+    app.get("/review", async (req, res) => {
+      console.log(req.query);
+      let query = {};
+      if (req.query.email) {
+        query = {
+          email: req.query.email,
+        };
+      }
+      const cursor = reviewCollection.find(query);
+      const review = await cursor.toArray();
+      res.send(review);
+    });
 
-        //review api
-        app.post('/review', async (req, res) => {
-            const review = req.body;
-            const result = await reviewCollection.insertOne(review);
-            res.send(result)
-        })
-
-
-    }
-    finally {
-        
-    }
+    //review post api
+    app.post("/review", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
+    });
+  } finally {
+  }
 }
-run().catch(err=> console.log(err))
+run().catch((err) => console.log(err));
 app.get("/", (req, res) => {
-    res.send("tour server running")
-})
+  res.send("tour server running");
+});
 
 app.listen(port, (req, res) => {
-    console.log(`server running ${port}`)
-})
+  console.log(`server running ${port}`);
+});
 
 // pass: qYTNkxT5XpUy8rx5
 // user = genius-user
-
